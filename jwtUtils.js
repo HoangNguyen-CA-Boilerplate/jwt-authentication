55b1f02422e2a45211267818bc1811db2702143a
Hoang <hoang.viet.nguyen@outlook.com>
2021-07-09 23:57:42 -0400

    Added custom middleware to verify JWT. Register, login, and get user workflow completed.

--- jwtUtils.js
+++ jwtUtils.js
@@ -1,4 +1,5 @@
 const jsonwebtoken = require('jsonwebtoken');
+const User = require('./models/User');
 const PRIV_KEY = Buffer.from(process.env.PRIV_KEY, 'base64').toString('utf-8');
 const PUB_KEY = Buffer.from(process.env.PUB_KEY, 'base64').toString('utf-8');
 
@@ -21,8 +22,31 @@ const issueJWT = (user) => {
   return `Bearer ${signedToken}`;
 };
 
-const authMiddleware = (req, res, next) => {
-  next(new AppError(401, 'You are not authorized.'));
+const authMiddleware = async (req, res, next) => {
+  try {
+    if (!req.header('authorization'))
+      throw new AppError(401, 'No token provided.');
+
+    const tokenParts = req.headers.authorization.split(' ');
+    if (
+      tokenParts[0] !== 'Bearer' ||
+      !tokenParts[1] ||
+      !tokenParts[1].match(/\S+\.\S+\.\S+/)
+    )
+      throw new AppError(401, 'Invalid token format.');
+
+    const tokenPayload = jsonwebtoken.verify(tokenParts[1], PUB_KEY, {
+      algorithms: ['RS256'],
+    });
+
+    const userId = tokenPayload.sub;
+    const user = await User.findById(userId);
+
+    req.user = user; // attach user to request object
+    next();
+  } catch (e) {
+    next(new AppError(401, 'Token is missing or invalid.'));
+  }
 };
 
 module.exports = {
